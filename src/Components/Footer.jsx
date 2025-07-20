d79714ba161103742e1fd7f03a03d13c7d4b1ddc
SRAYA SREEDHARAN <srayasreedharan111@gmail.com>
2025-07-20 20:49:31 +0530

    start

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import  '../Components/Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="siteInfo">
+      <div className="copyRight">© 2025 All rights reserved.</div>
+      <div className="creator">Created By Sraya Sreedharan K</div>
+    </div>
+  );
+};
+
+export default Footer;
